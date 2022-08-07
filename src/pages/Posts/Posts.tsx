9815fe1a68e31c2a6388b8c9691890750e34060b
henricker <henriquevieira@alu.ufc.br>
2022-08-07 19:27:00 -0300

    feat: add post mocked on container of posts

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,11 +1,27 @@
 import { Sidebar } from "../../components/Sidebar";
-import { ContentContainer, PostsContainer } from "./style";
+import { Post } from "./Post";
+import { AllPostsContainer, ContentContainer, PostsContainer } from "./style";
+import AvatarMocked2 from "../../assets/mock/avatar2.svg";
+
+const dateMocked = new Date("2022-08-07 00:00:00");
 
 export const Posts = () => {
   return (
     <PostsContainer>
       <ContentContainer>
         <Sidebar />
+        <AllPostsContainer>
+          <Post
+            profile={{
+              name: "Jane Cooper",
+              profession: "Dev Front-End",
+              publishedAt: dateMocked,
+              srcAvatar: AvatarMocked2,
+            }}
+            portfolioLink="https://www.google.com"
+            text={`Fala galeraa 👋. Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀`}
+          />
+        </AllPostsContainer>
       </ContentContainer>
     </PostsContainer>
   );
--- src/pages/Posts/style.ts
+++ src/pages/Posts/style.ts
@@ -5,7 +5,12 @@ export const PostsContainer = styled.div``;
 export const ContentContainer = styled.div`
   display: flex;
   flex-direction: row;
-  width: calc(100% - 10rem);
+  width: calc(100% - 20rem);
   margin: 0 auto;
   margin-top: 2rem;
+`;
+
+export const AllPostsContainer = styled.div`
+  margin-left: auto;
+  width: 100%;
 `;
