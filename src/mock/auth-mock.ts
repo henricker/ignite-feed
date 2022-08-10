932a62464896c76c80f1f8b3d6b8af5aec0143b5
henricker <henriquevieira@alu.ufc.br>
2022-08-10 10:34:13 -0300

    feat: add auth mock

---
+++ src/mock/auth-mock.ts
@@ -0,0 +1,13 @@
+import MockedAvatar from "../assets/mock/avatar.png";
+
+export const authMock = {
+  isAuthenticated: true,
+  profile: {
+    name: "Henrique Vieira",
+    email: "henricker@email.com",
+    srcAvatar: MockedAvatar,
+    profession: "Software Engineer",
+  },
+  jwtToken: "1201390skjdkadja",
+  refreshToken: "skdksdj29i01",
+};
