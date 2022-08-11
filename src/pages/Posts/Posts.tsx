f2b129afb1c30554abc4980128587b8540345476
henricker <henriquevieira@alu.ufc.br>
2022-08-11 11:17:32 -0300

    refactor: show sidebar if width screen greater than 670 px

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,18 +1,17 @@
 import { Sidebar } from "../../components/Sidebar";
 import { Post } from "./Post";
 import { AllPostsContainer, ContentContainer, PostsContainer } from "./style";
-import { usePosts, UsePostsProvider } from "./hooks/usePosts";
+import { usePosts } from "./hooks/usePosts";
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
 
 export const Posts = () => {
   const { posts } = usePosts();
-
   const { width } = useWindowDimensions();
 
   return (
     <PostsContainer>
       <ContentContainer>
-        {width >= 670 && <Sidebar />}
+        {width > 670 && <Sidebar />}
         <AllPostsContainer>
           {posts.map((post) => (
             <Post post={post} key={post.id} />
