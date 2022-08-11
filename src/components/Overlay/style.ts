f339560fc16dcb0f919264d56d63ca441d0ad837
henricker <henriquevieira@alu.ufc.br>
2022-08-11 11:16:05 -0300

    feat: add overlay component

---
+++ src/components/Overlay/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
