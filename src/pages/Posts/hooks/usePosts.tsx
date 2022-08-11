12de15e978ba5c4df27c7490e7467a9f2692c46c
henricker <henriquevieira@alu.ufc.br>
2022-08-11 10:01:46 -0300

    refactor: sort mocks posts

--- src/pages/Posts/hooks/usePosts.tsx
+++ src/pages/Posts/hooks/usePosts.tsx
@@ -1,7 +1,8 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useCallback, useContext, useState } from "react";
 import { AuthContext } from "../../../contexts/auth";
 import { CommentPostType, PostType } from "../../../interfaces/posts";
 import AvatarMocked2 from "../../../assets/mock/avatar2.svg";
+import AvatarMocked3 from "../../../assets/mock/avatar3.png";
 
 type UsePostsContextProps = {
   posts: PostType[];
@@ -20,6 +21,33 @@ const UsePostsContext = createContext({} as UsePostsContextProps);
 export const UsePostsProvider = ({ children }: { children: any }) => {
   const auth = useContext(AuthContext);
   const [posts, setPosts] = useState([
+    {
+      id: 2,
+      profile: {
+        name: "Jim Brown",
+        profession: "Dev Back-End",
+        publishedAt: new Date("2022-08-10 00:00:00"),
+        srcAvatar: AvatarMocked3,
+      },
+      portfolioLink: "jane.design/discordBotMusic",
+      text: `Hi bros! 👋. I finish my project, your name is youtube discord bot!🚀`,
+      hashtags: ["#newbot", "#nlw", "#everybotting"],
+      comments: [
+        {
+          profile: {
+            srcAvatar: auth.profile.srcAvatar,
+            name: "Henrique Vieira",
+            profession: "Web Developer",
+          },
+          post_id: 2,
+          comment: "Very good ma babe",
+          likes: 10,
+          publishedAt: new Date("2022-08-08 13:00:00"),
+          id: 1,
+          isMe: true,
+        },
+      ],
+    },
     {
       id: 1,
       profile: {
@@ -49,29 +77,38 @@ export const UsePostsProvider = ({ children }: { children: any }) => {
     },
   ]);
 
-  function handleAddComment(comment: CommentPostType) {
-    const post = { ...posts.find((post) => post.id === comment.post_id)! };
-    post.comments.push(comment as any);
-    const postsFilter = posts.filter((post) => post.id !== comment.post_id);
+  const handleAddComment = useCallback(
+    (comment: CommentPostType) => {
+      const post = { ...posts.find((post) => post.id === comment.post_id)! };
+      post.comments.push(comment as any);
+      const postsFilter = posts.filter((post) => post.id !== comment.post_id);
 
-    setPosts([...postsFilter, post]);
-  }
+      setPosts([...postsFilter, post].sort((a, b) => b.id - a.id));
+    },
+    [posts]
+  );
 
-  function handleDeleteComment({
-    comment_id,
-    post_id,
-  }: {
-    post_id: number | string;
-    comment_id: number | string;
-  }) {
-    const post = { ...posts.find((post) => post.id === post_id)! };
-    post.comments = post.comments.filter(
-      (comment) => comment.id !== comment_id
-    );
-    const postsFilter = posts.filter((post) => post.id !== post_id);
+  const handleDeleteComment = useCallback(
+    ({
+      comment_id,
+      post_id,
+    }: {
+      post_id: number | string;
+      comment_id: number | string;
+    }) => {
+      console.log(comment_id, post_id);
+      const post = { ...posts.find((post) => post.id === post_id)! };
+      post.comments = post.comments.filter(
+        (comment) => comment.id !== comment_id
+      );
 
-    setPosts([...postsFilter, post]);
-  }
+      console.log(post.comments);
+      const postsFilter = posts.filter((post) => post.id !== post_id);
+
+      setPosts([...postsFilter, post].sort((a, b) => b.id - a.id));
+    },
+    [posts]
+  );
 
   return (
     <UsePostsContext.Provider
